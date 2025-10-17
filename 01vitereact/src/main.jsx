ba82baef33dca2b858bbdbd59401231501af5f83
neetu573 <146418374+neetu573@users.noreply.github.com>
2025-10-17 22:52:22 +0530

    custom

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom Vite</h1>
+    </div>
+  )
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://www.google.com',
+        target: '_blank',
+    },
+    children: 'Click me to go to Google',
+}
+
+const anotherElement = (
+  <a href="https://www.example.com" target="_blank">
+    Visit Google(29.40)
+  </a>
+)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <MyApp />
     <App />
+    {anotherElement}
   </StrictMode>,
 )
