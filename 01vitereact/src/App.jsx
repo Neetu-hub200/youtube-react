77fe78f98922e234ce55b96b3ddb8b6ffd579745
neetu573 <146418374+neetu573@users.noreply.github.com>
2025-10-17 21:58:34 +0530

    chai-code

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,12 @@
-
+import Chai from "./chai";
 
 function App() {
 
   return (
-      <h1>Hello, World! with vite</h1>
+    <>
+    <Chai />
+    </>
   )
 }
 
-export default App
+export default App;
