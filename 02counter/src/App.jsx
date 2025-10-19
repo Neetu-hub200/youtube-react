de959b5763f728ed6b2341988b4767ef18ee0f3e
neetu573 <146418374+neetu573@users.noreply.github.com>
2025-10-19 23:52:35 +0530

    hooks and tailwindcss

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [counter, setCounter] = useState(10);
+
+  // let counter = 5;
+
+  const addValue = () => {
+    // counter = counter + 1;
+    setCounter(counter + 1);
+    // console.log("clicked", counter);
+    }
+
+  const removeValue = () => {
+    setCounter(counter - 1);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Chai aur React</h1>
+    <h2>Counter value: {counter}</h2>
+
+    <button onClick={addValue}>Add Value {counter}</button>
+    <br/>
+    <button onClick={removeValue}>Remove Value {counter}</button>
+    <p>Footer {counter}</p>
     </>
   )
 }
