bf6a1d45f1e70ac711e00fd87ee90a2805a9f5f9
neetu573 <146418374+neetu573@users.noreply.github.com>
2025-10-20 22:47:09 +0530

    cardchanges

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,27 +1,23 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Card from "./components/card.jsx";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  let myObj = {
+    name : "neetu",
+    age: 23
+  }
+  let newArray = [1,2,3];
   return (
     <>
       <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
         Tailwind Test
       </h1>
-      <div className="w-60 h-60 rounded-xl">
-        <img
-          src="https://media1.giphy.com/media/z8n8dWgQ0mgEIyzlmV/giphy.gif?cid=790b7611a5ba988db1bc7457636dd163c28af6f6dbc84a77&rid=giphy.gif&ct=g"
-          alt="Giphy"
-          className="rounded-t-xl"
-        />
-        <div className="glass py-4 px-5 relative -top-[3.4rem]  rounded-b-xl z-10">
-          <h1 className="font-bold  font-mono  text-xl">Available soon</h1>
-          14:20 
-        </div>
-      </div>
+      <Card  username="Avenger" btnText="Click Me" />
+      <Card  username="Ironman"/>
     </>
   );
 }
